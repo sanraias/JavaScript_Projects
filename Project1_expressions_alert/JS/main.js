57d306fee91ae6cfe212bb1a64560a3c7858c31e
sanraias <svasco_1982@hotmail.com>
2024-10-01 13:04:30 +0100

    Commiting changes

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,1 +1,24 @@
-alert ("Hello, World!");
+var A = "This is a string"; //Defining variable A
+document.write (A); //Printing the value on screen
+
+var B = "Welcome to my webpage!"; //Defining variable B
+window.alert(B); //Using an expression to show the variable in a pop up window
+
+document.write ("This is a backlash\\. It is used to escape characters.<br>For example if I want to say\:\"Hello, World! I can't go.\""); //Printing a variable using the backlashes
+
+document.write ("\"If you want to know what a man\'s like, take a good look at" //Printing on screen. Concatenating a string using +
++ " how he treats his inferiors, not his equals.\""
++ "- J.K. Rowling, Harry Potter and the Goblet of Fire");
+
+var C = "\"I've learned that people will forget what you said, people will" //Defining variable C and printing on screen
++ " forget what you did, but people will never forget"
++ " how you made them feel."
++ "― Maya Angelou";
+document.write(C)
+
+var Family = "The Smiths", Dad = "John", Mom = "Diana", //Defining variable Family and printing a family member
+    Daughter = "Carly", Son = "Pepe";
+    document.write(Dad);
+
+var D = "3+3"; //Defining an expression
+document. write(D);
