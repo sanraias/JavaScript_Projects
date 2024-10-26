2b6f02e2f12ef67b7aaca5d369d7e1625ef7992a
sanraias <svasco_1982@hotmail.com>
2024-10-26 17:24:17 +0100

    Commiting

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,19 @@
+var x = 25;
+function Add_numbers_1() {
+    document.write(25 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(x + 100 + "<br>");
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_3() {
+    var y = 45;
+    document.write(100 - x + "<br>");
+}
+function Add_numbers_4() {
+    document.write(y + 29);
+}
+Add_numbers_3();
+Add_numbers_4();
